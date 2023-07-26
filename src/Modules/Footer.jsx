c171d135f13eee3c635a26e364eaacaf7780bc04
Nathan Williams <nate88williams@gmail.com>
2023-07-26 18:16:24 -0500

    Created footer and placed it at the bottom of page. Need to ensure parent div has className='flex flex-col min-h-screen' for it to work properly.

---
+++ src/Modules/Footer.jsx
@@ -0,0 +1,12 @@
+
+function Footer() {
+    return (
+        <div className='mt-auto'>
+            <div className='h-28 bg-[#7d98a1] text-white'>
+                Hello World!
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
