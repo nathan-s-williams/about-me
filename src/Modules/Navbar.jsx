ce7e0db83b896dc21c65adfef3fcff5d8ee46050
Nathan Williams <nate88williams@gmail.com>
2023-07-26 20:20:05 -0500

    Changed the custom text size of signature to 3xl.

--- src/Modules/Navbar.jsx
+++ src/Modules/Navbar.jsx
@@ -2,7 +2,7 @@
 function Navbar() {
     return (
         <div className='flex items-center h-24 w-full bg-[#1c2321]'>
-            <p className='flex justify-center flex-1 min-w-[250px] text-white text-[30px]'>Nathan Williams</p>
+            <p className='flex justify-center flex-1 min-w-[250px] text-white text-3xl'>Nathan Williams</p>
             <div className='flex justify-center flex-1 text-slate-300 min-w-[450px]'>
                 <a className='flex justify-center flex-1' href={'#'}>Home</a>
                 <a className='flex justify-center flex-1' href={'#'}>Career</a>
