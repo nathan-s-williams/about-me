97c7f2a314c0a05c5b6a703ff5718c39c305b09f
Nathan Williams <nate88williams@gmail.com>
2023-07-27 18:52:15 -0500

    Created first version of Footer. Considering changing the links to icons or moving them to make it more aesthetically pleasing.

--- src/Modules/Footer.jsx
+++ src/Modules/Footer.jsx
@@ -1,9 +1,15 @@
 
 function Footer() {
     return (
-        <div className='mt-auto'>
-            <div className='h-28 bg-[#7d98a1] text-white'>
-                Hello World!
+        <div className='flex justify-center items-center mt-auto h-28 bg-[#7d98a1] text-white text-xs'>
+            <p className='basis-7/12 min-w-[260px] ml-3'>&copy; 2023 Created by me... for those interested</p>
+            <div className='flex justify-center flex-1 text-slate-300 min-w-[330px]'>
+                <a className='flex justify-center flex-1 border-r-2' href={'#'}>Home</a>
+                <a className='flex justify-center flex-1 border-r-2' href={'#'}>Career</a>
+                <a className='flex justify-center flex-1 border-r-2' href={'#'}>School</a>
+                <a className='flex justify-center flex-1 border-r-2' href={'#'}>Hobbies</a>
+                <a className='flex justify-center flex-1 border-r-2' href={'#'}>Contact</a>
+                <a className='flex justify-center flex-1' href={'#'}>This Site</a>
             </div>
         </div>
     );
