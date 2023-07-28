fe93baf73d4f48599d58a8a249a023c2b8efc9cf
Nathan Williams <nate88williams@gmail.com>
2023-07-27 19:47:28 -0500

    Added popup text for each icon link to specify its destination.

--- src/Modules/Footer.jsx
+++ src/Modules/Footer.jsx
@@ -10,11 +10,11 @@ function Footer() {
         <div className='flex justify-center items-center mt-auto h-28 bg-[#7d98a1] text-white text-xs'>
             <p className='basis-5/12 min-w-[260px] ml-3'>&copy; 2023 Created by me... for those interested</p>
             <div className='flex justify-center flex-1 basis-2/12 text-slate-300 min-w-[330px]'>
-                <a className='flex justify-center flex-1' href={'#'}><HomeIcon /></a>
-                <a className='flex justify-center flex-1' href={'#'}><WorkIcon /></a>
-                <a className='flex justify-center flex-1' href={'#'}><SchoolIcon /></a>
-                <a className='flex justify-center flex-1' href={'#'}><SportsBasketballIcon /></a>
-                <a className='flex justify-center flex-1' href={'#'}><CallIcon /></a>
+                <a className='flex justify-center flex-1' href={'#'} title='Home'><HomeIcon /></a>
+                <a className='flex justify-center flex-1' href={'#'} title='Career'><WorkIcon /></a>
+                <a className='flex justify-center flex-1' href={'#'} title='School'><SchoolIcon /></a>
+                <a className='flex justify-center flex-1' href={'#'} title='Hobbies'><SportsBasketballIcon /></a>
+                <a className='flex justify-center flex-1' href={'#'} title='Contact'><CallIcon /></a>
             </div>
             <div className='basis-5/12'></div>
         </div>
