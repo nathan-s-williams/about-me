8232a8f39d08d19e7e66c70d443b14ab30d9dc0a
Nathan Williams <nate88williams@gmail.com>
2023-07-31 19:44:30 -0500

    Changed the reference to Homepage.jsx to its new directory location.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import BoilerPlate from './Modules/BoilerPlate.jsx'
-import Homepage from './Modules/Homepage.jsx'
+import Homepage from './Modules/PageContent/Homepage.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
