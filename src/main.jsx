667cd59648ede363d467dd6e1325eee9e322e33c
Nathan Williams <nate88williams@gmail.com>
2023-08-02 00:16:52 -0500

    Added routes to main.jsx for all pages. Main path is / and passes content pages as children based on the path.

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import BoilerPlate from './Modules/BoilerPlate.jsx'
-import Homepage from './Modules/PageContent/Homepage.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import BoilerPlate from './Modules/BoilerPlate.jsx';
+import Homepage from './Modules/PageContent/Homepage.jsx';
+import Career from './Modules/PageContent/Career.jsx';
+import School from './Modules/PageContent/School.jsx';
+import Hobbies from './Modules/PageContent/Hobbies.jsx';
+import Contact from './Modules/PageContent/Contact.jsx';
 import './index.css'
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <BoilerPlate />,
+        children: [
+            { index: true, element: <Homepage /> },
+            { path: 'career', element: <Career /> },
+            { path: 'school', element: <School /> },
+            { path: 'hobbies', element: <Hobbies /> },
+            { path: 'contact', element: <Contact /> }
+        ],
+    },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <BoilerPlate content={<Homepage />} />
+        <RouterProvider router={router} />
     </React.StrictMode>,
-)
+);
