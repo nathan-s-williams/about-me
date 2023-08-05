7df29e4faa32e0b00eb80f8895b6067dff9e39c2
Nathan Williams <nate88williams@gmail.com>
2023-08-05 10:55:53 -0500

    Changed the a tag to Link components to facilitate the react router dom.

--- src/Modules/Navbar.jsx
+++ src/Modules/Navbar.jsx
@@ -1,15 +1,14 @@
-
+import { Link } from 'react-router-dom';
 function Navbar() {
     return (
         <div className='flex items-center h-24 w-full bg-[#1c2321]'>
             <p className='flex justify-center flex-1 min-w-[250px] text-white text-3xl'>Nathan Williams</p>
             <div className='flex justify-center flex-1 text-slate-300 min-w-[450px]'>
-                <a className='flex justify-center flex-1' href={'#'}>Home</a>
-                <a className='flex justify-center flex-1' href={'#'}>Career</a>
-                <a className='flex justify-center flex-1' href={'#'}>School</a>
-                <a className='flex justify-center flex-1' href={'#'}>Hobbies</a>
-                <a className='flex justify-center flex-1' href={'#'}>Contact</a>
-                <a className='flex justify-center flex-1' href={'#'}>This Site</a>
+                <Link className='flex justify-center flex-1' to='/'>Home</Link>
+                <Link className='flex justify-center flex-1' to='/career'>Career</Link>
+                <Link className='flex justify-center flex-1' to='/school'>School</Link>
+                <Link className='flex justify-center flex-1' to='/hobbies'>Hobbies</Link>
+                <Link className='flex justify-center flex-1' to='/contact'>Contact</Link>
             </div>
         </div>
     );
